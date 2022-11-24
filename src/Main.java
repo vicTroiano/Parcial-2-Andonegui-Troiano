becd2e58ceb39060eb6048bb666c61fc4f4482f3
vicTroiano <115674802+vicTroiano@users.noreply.github.com>
2022-11-24 16:38:11 -0300

    feat: refactor main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Shape s1 = new Circle(5.5,"red",false);
-        Shape s2 = new Square(); //carga usando setters
+        Shape s2 = new Rectangle(); //rectangulo usando setter
         Shape s3 = new Rectangle(1.0, 2.0, "red", false);
         Shape s4 = new Square(6.6);
         Shape s5 = new Shape() { //chequear - me crea override
@@ -21,6 +21,7 @@ public class Main {
         System.out.println(s3);
         System.out.println(s4);
         System.out.println(s5);
+
 
     }
 }
